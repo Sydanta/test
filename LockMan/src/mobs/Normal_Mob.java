dd9cb7d63541b32562fd216cebd26fe742648167
cnnfr <cnnfr@free.fr>
2013-09-04 18:22:00 +0200

    test

---
+++ LockMan/src/mobs/Normal_Mob.java
@@ -0,0 +1,13 @@
+package mobs;
+
+import org.andengine.opengl.texture.region.ITiledTextureRegion;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
+
+public class Normal_Mob extends General_Mob {
+
+	public Normal_Mob(float pX, float pY, float pWidth, float pHeight, ITiledTextureRegion pTiledTextureRegion, VertexBufferObjectManager pVertexBufferObjectManager, Mob_Type pMob_type) {
+		super(pX, pY, pWidth, pHeight, pTiledTextureRegion, pVertexBufferObjectManager, pMob_type);
+		// TODO Auto-generated constructor stub
+	}
+
+}
